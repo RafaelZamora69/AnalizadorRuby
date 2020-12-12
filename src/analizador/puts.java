a141ff5d04485c205034e631f1c0e4208abe59ae
Rafael Zamora <rafael_zamora99@hotmail.com>
2020-12-11 18:19:51 -0600

    Puts

--- src/analizador/puts.java
+++ src/analizador/puts.java
@@ -2,7 +2,11 @@ import java.util.HashMap;
 
 public class puts {
     public static HashMap<String, Object> variables = tokenAsignaciones.obtenerHashMap();
-    public static void puts(Token t1){
-        System.out.println(variables.get(t1.image));
+    public static void imprimirVariable(Token t1){
+        System.out.println(variables.get(t1.image) == null ? "Error: '" + t1.image + "' no existe" : variables.get(t1.image));
+    }
+
+    public static void imprimir(String text){
+        System.out.println(text);
     }
 }
