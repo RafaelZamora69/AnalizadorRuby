c83d6eb2b406e6817d7d429c14ff4911b3b36ea3
Rafael Zamora <rafael_zamora99@hotmail.com>
2021-01-16 01:54:12 -0600

    Wip

--- src/analizador/comparacion.java
+++ src/analizador/comparacion.java
@@ -3,12 +3,11 @@ import java.util.HashMap;
 public class comparacion {
 
     public static void CompararEnteros(Object t1, Object t2, String operador){
-        //Intentar convertir Objetos a enteros
         try{
             t1 = Integer.parseInt((String)t1);
             t2 = Integer.parseInt((String)t2);
         }catch(Exception e){
-            System.out.println("Error semántico: \"" + (String)t1 + "\" no puede ser comparado con Integer");
+            System.out.println("Error semantico: \"" + (String)t1 + "\" no puede ser comparado con Integer");
             return;
         }
         switch (operador) {
@@ -51,21 +50,40 @@ public class comparacion {
     }
 
     public static void CompararAsignacion(Object t1, String t2, String operador){
-        //TODO: Cambiar arreglo, debe de guardarse como ArrayList. Comparar booleanos
+        //TODO: Cambiar arreglo, debe de guardarse como ArrayList.
         HashMap<String,Object> variables = tokenAsignaciones.obtenerHashMap();
         if(variables.get(t2) != null){
             Object variable = variables.get(t2);
-            System.out.println(variable);
+            System.out.println(variable.getClass());
             if(variable instanceof Integer){
                 CompararEnteros(t1, (Integer)variable, operador);
                 return;
             }
             if(variable instanceof StringBuilder){
                 CompararCadenas((String)t1, variable.toString(), operador);
+                return;
+            }
+            if(variable instanceof Boolean){
+                CompararBoolean((String)t1, variable.toString(), operador);
             }
         } else {
             System.out.println("Error: \"" + t2 + " no existe");
         }
         variables.get(t2);
+    }
+
+    public static void CompararBoolean(Object t1, Object t2, String operador){
+        try{
+            t1 = Boolean.parseBoolean((String)t1);
+            t2 = Boolean.parseBoolean((String)t2);
+        }catch(Exception e){
+            System.out.println("Error semantico: \"" + (String)t1 + "\" no puede ser comparado con Integer");
+            return;
+        }
+        if(operador.equals("==") || operador.equals("!=")){
+            System.out.println(operador.equals("==") ? (Boolean)t1 == (Boolean)t2 : (Boolean)t1 != (Boolean)t2);
+        } else {
+            System.out.println("Error semantico: " + operador + " no es un operador valido");
+        }
     }
 }
